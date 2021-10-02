d835cc11262774a209e541ea79daa993642d879b
Nikhil Tiwari <87422608+Nikhil8144@users.noreply.github.com>
2021-10-02 16:38:29 +0530

    Update notify.js

--- notify.js
+++ notify.js
@@ -1,7 +1,28 @@
-Notification.requestPermission().then(function (permission) {
-    console.log(permission);
-});
-var title = "Neptune - Chat";
-icon = '';
-var body = "New Notification, ";
-var notification = new Notification(title, { body, icon });
+let permission = Notification.permission;
+if(permission === "granted") {
+   showNotification();
+} else if(permission === "default"){
+   requestAndShowPermission();
+} else {
+  alert("Use normal alert");
+}
+function showNotification() {
+   if(document.visibilityState === "visible) {
+       return;
+   }
+   var title = "JavaScript Jeep";
+   icon = "image-url"
+   var body = "Message to be displayed";
+   var notification = new Notification('Title', { body, icon });
+   notification.onclick = () => { 
+          notification.close();
+          window.parent.focus();
+   }
+}
+function requestAndShowPermission() {
+   Notification.requestPermission(function (permission) {
+      if (permission === "granted") {
+            showNotification();
+      }
+   });
+}

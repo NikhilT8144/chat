77a065e056ff015d19859fb0941161e5f58084ca
Nikhil Tiwari <87422608+Nikhil8144@users.noreply.github.com>
2021-10-02 16:30:05 +0530

    Update notify.js

--- notify.js
+++ notify.js
@@ -1,1 +1,7 @@
-
+Notification.requestPermission().then(function (permission) {
+    console.log(permission);
+});
+var title = "Neptune - Chat";
+icon = '';
+var body = "New Notification, ";
+var notification = new Notification(title, { body, icon });

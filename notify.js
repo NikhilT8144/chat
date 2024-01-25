17a51d37f9cfe4f41788bb034f567808f2e28d22
Nikhil Tiwari <87422608+NikhilT8144@users.noreply.github.com>
2024-01-25 20:05:49 +0530

    Update notify.js

--- notify.js
+++ notify.js
@@ -10,9 +10,9 @@ function showNotification() {
    if(document.visibilityState === "visible) {
        return;
    }
-   var title = "JavaScript Jeep";
-   icon = "image-url"
-   var body = "Message to be displayed";
+   var title = "Neptune - Chat";
+   icon = "icon.png"
+   var body = "Checking for new messages...";
    var notification = new Notification('Title', { body, icon });
    notification.onclick = () => { 
           notification.close();
